14dc66ceca216065bcde50f5263846629543eb37
Renan Fritsch <bfritschrenan@gmail.com>
2025-12-24 14:55:16 -0300

    feat: cria a classe Despesa com a status e parcelas

---
+++ src/domain/despesa.py
@@ -0,0 +1,20 @@
+class Despesa:
+    def __init__(self, 
+                 descricao: str, 
+                 valor: float, 
+                 categoria: str, 
+                 data_vencimento: str, 
+                 tipo: str = "UNICA", 
+                 status: str = "PENDENTE", 
+                 total_parcelas: int = None, 
+                 parcela_atual: int = None):
+
+        self.descricao = descricao
+        self.valor = valor
+        self.categoria = categoria
+        self.data_vencimento = data_vencimento
+        self.status = status
+        self.tipo = tipo.upper()
+        self.total_parcelas = total_parcelas 
+        self.parcela_atual = parcela_atual
+        
