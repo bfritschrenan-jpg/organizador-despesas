a3c70f3af918c5fdc68bb99b3477bce38cd28d2d
Renan Fritsch <bfritschrenan@gmail.com>
2025-12-25 16:27:04 -0300

    feat: configurar DatabaseManager com tabelas de categorias/despesas e persistência

---
+++ src/infrastructure/database_manager.py
@@ -0,0 +1,59 @@
+import sqlite3
+from src.domain.categoria import Categoria
+
+
+class DatabaseManager:
+    def __init__(self, db_name="organizador.db"):
+        self.db_name = db_name
+        self.criar_tabelas()
+
+    def conectar(self):
+        """Cria uma conexão com o banco de dados."""
+        return sqlite3.connect(self.db_name)
+
+    def criar_tabelas(self):
+        """Cria as tabelas de Categoria e Despesa se elas não existirem."""
+        conn = self.conectar()
+        cursor = conn.cursor()
+
+        # Tabela de Categorias
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS categorias (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nome TEXT NOT NULL,
+                cor TEXT
+            )
+        ''')
+
+        # Tabela de Despesas
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS despesas (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                descricao TEXT NOT NULL,
+                valor REAL NOT NULL,
+                data_vencimento TEXT,
+                status TEXT,
+                tipo TEXT,
+                total_parcelas INTEGER,
+                parcela_atual INTEGER,
+                categoria_id INTEGER,
+                FOREIGN KEY (categoria_id) REFERENCES categorias (id)
+            )
+        ''')
+
+        conn.commit()
+        conn.close()
+
+    def salvar_categoria(self, categoria):
+        conn = self.conectar()
+        cursor = conn.cursor()
+
+        cursor.execute('''
+            INSERT INTO categorias (nome, cor)  VALUES (?, ?)
+        ''',
+        (categoria.nome, categoria.cor)
+        )
+       
+       
+        conn.commit()
+        conn.close()
