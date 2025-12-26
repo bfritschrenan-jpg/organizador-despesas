7518246155ebee230f184a9c2d20e39b9bafca9f
Renan Fritsch <bfritschrenan@gmail.com>
2025-12-26 15:25:53 -0300

    feat: implementar CategoriaService com validacoes de negocio

---
+++ src/services/categoria_service.py
@@ -0,0 +1,26 @@
+from src.infrastructure.database_manager import DatabaseManager
+from src.domain.categoria import Categoria
+class CategoriaService:
+    def __init__(self, db_manager):
+        self.db = db_manager
+    
+    def cadastrar_categoria(self,nome,cor):
+
+        nome_categoria = nome.strip()
+
+        if not nome_categoria:
+            return False, "Erro: A categoria precisa de um nome!"
+        
+        nome_existe = self.db.buscar_categoria_por_nome(nome_categoria)
+
+        if nome_existe is not None:
+            return False, f"Erro: A categoria '{nome_categoria}' já existe!"
+        
+        if cor and cor.strip():
+            nova_categoria = Categoria(nome=nome_categoria, cor=cor)
+        else:
+            nova_categoria = Categoria(nome=nome_categoria)
+            
+        self.db.salvar_categoria(nova_categoria)
+
+        return True, f"Sucesso: Categoria '{nome_categoria}' cadastrada!"
